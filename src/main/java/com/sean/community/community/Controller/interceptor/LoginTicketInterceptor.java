a76ed22f08e31161393cbe24950150fe80fe0d3c
Sean <sean.yancy@outlook.com>
2023-03-02 16:41:06 +0800

    Add: User Login Status presented

---
+++ src/main/java/com/sean/community/community/Controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,58 @@
+package com.sean.community.community.Controller.interceptor;
+
+import com.sean.community.community.entity.LoginTicket;
+import com.sean.community.community.entity.User;
+import com.sean.community.community.service.UserService;
+import com.sean.community.community.util.CookieUtil;
+import com.sean.community.community.util.HostHolder;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 从cookie中获取凭证
+        String ticket = CookieUtil.getValue(request, "ticket");
+
+        if (ticket != null) {
+            // 查询ticket
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            //
+            if (loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())) {
+                // 根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                // 本次请求中持有用户
+                hostHolder.setUser(user);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            modelAndView.addObject("loginUser", user);
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        hostHolder.clear();
+    }
+}
