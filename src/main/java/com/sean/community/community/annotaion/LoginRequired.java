f69a9625d2344c4f7c61e84cd6df6499368736f6
Sean <sean.yancy@outlook.com>
2023-03-13 13:11:07 +0800

    Add: Login State check

---
+++ src/main/java/com/sean/community/community/annotaion/LoginRequired.java
@@ -0,0 +1,11 @@
+package com.sean.community.community.annotaion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginRequired { // 只是一个标记，不需要内容
+}
--- src/main/java/com/sean/community/community/config/WebMvcConfig.java
+++ src/main/java/com/sean/community/community/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.sean.community.community.config;
 
+import com.sean.community.community.Controller.interceptor.LoginRequiredInterceptor;
 import com.sean.community.community.Controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
+
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
+        registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
     }
 
