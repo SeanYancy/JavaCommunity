6e8ae501989660f8ee7ae1ab2e9c82c1940cd456
Sean <sean.yancy@outlook.com>
2023-03-14 10:47:38 +0800

    Add: Show comments of a post

--- src/main/java/com/sean/community/community/Controller/DiscussPostController.java
+++ src/main/java/com/sean/community/community/Controller/DiscussPostController.java
@@ -1,10 +1,14 @@
 package com.sean.community.community.Controller;
 
 
+import com.sean.community.community.entity.Comment;
 import com.sean.community.community.entity.DiscussPost;
+import com.sean.community.community.entity.Page;
 import com.sean.community.community.entity.User;
+import com.sean.community.community.service.CommentService;
 import com.sean.community.community.service.DiscussPostService;
 import com.sean.community.community.service.UserService;
+import com.sean.community.community.util.CommunityConstant;
 import com.sean.community.community.util.CommunityUtil;
 import com.sean.community.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
@@ -29,6 +33,9 @@ public class DiscussPostController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -51,11 +58,62 @@ public class DiscussPostController {
     }
 
     @RequestMapping(path="/detail/{discussPostId}", method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int postId, Model model) {
+    public String getDiscussPost(@PathVariable("discussPostId") int postId, Model model, Page page) {
         DiscussPost post = discussPostService.findDiscussPostById(postId);
         model.addAttribute("post", post);
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user", user);
+
+        // 设置分页
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + postId);
+        page.setRows(post.getCommentCount());
+
+
+        // 评论：给帖子的评论
+        // 回复：给评论的评论
+        // 评论的列表
+        List<Comment> commentList = commentService.findCommentsByEntity(ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论的VO列表（VO显示对象）
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment: commentList) {
+                // 单个评论的VO
+                Map<String, Object> commentVo = new HashMap<>();
+                // 添加评论
+                commentVo.put("comment", comment);
+                // 添加评论的作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // 查询回复
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+
+                // 回复的VO列表
+                List<Map<String, Object>> replayVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply: replyList){
+                        Map<String, Object> replayVo = new HashMap<>();
+                        replayVo.put("reply", reply);
+                        replayVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复的目标
+                        User target = reply.getTargetId() == 0? null: userService.findUserById(reply.getUserId());
+                        replayVo.put("target", target);
+
+                        replayVoList.add(replayVo);
+                    }
+                }
+                // 把回复放入评论VO
+                commentVo.put("replays", replayVoList);
+
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
 
         return "/site/discuss-detail";
     }
---
+++ src/main/java/com/sean/community/community/dao/CommentMapper.java
@@ -0,0 +1,14 @@
+package com.sean.community.community.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.sean.community.community.entity.Comment;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    int selectCountByEntity(int entityType, int entityId);
+
+}
