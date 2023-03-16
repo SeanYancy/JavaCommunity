697879fdada5e0c7be4f70287d2a392ab563dde3
Sean <sean.yancy@outlook.com>
2023-03-16 16:28:34 +0800

    REFACTOR: When querying users, the returned information will be stored in redis as cache to improve performance.

--- src/main/java/com/sean/community/community/util/RedisKeyUtil.java
+++ src/main/java/com/sean/community/community/util/RedisKeyUtil.java
@@ -8,6 +8,7 @@ public class RedisKeyUtil {
     private static final String PREFIX_FOLLOWER = "follower";
     private static final String PREFIX_KAPTCHA = "kaptcha";
     private static final String PREFIX_TICKET = "ticket";
+    private static final String PREFIX_USER = "user";
 
 
 
@@ -45,4 +46,9 @@ public class RedisKeyUtil {
     public static String getTicketKey(String ticket) {
         return PREFIX_TICKET + SPLIT + ticket;
     }
+
+    public static String getUserKey(int userId) {
+        return PREFIX_USER + SPLIT + userId;
+    }
+
 }
