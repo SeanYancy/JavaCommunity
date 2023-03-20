0dfcc95ceae682f061270c0653878ae3dbc83a7e
Sean <sean.yancy@outlook.com>
2023-03-20 15:54:02 +0800

    ADD: Notice Detail display

--- src/main/java/com/sean/community/community/config/WebMvcConfig.java
+++ src/main/java/com/sean/community/community/config/WebMvcConfig.java
@@ -2,6 +2,8 @@ package com.sean.community.community.config;
 
 import com.sean.community.community.Controller.interceptor.LoginRequiredInterceptor;
 import com.sean.community.community.Controller.interceptor.LoginTicketInterceptor;
+import com.sean.community.community.Controller.interceptor.MessageInterceptor;
+import com.sean.community.community.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,10 +17,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.png", "/**/*.jpeg");
     }
 
